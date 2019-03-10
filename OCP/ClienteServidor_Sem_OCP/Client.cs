4444fd15d5a30aa544ae1092376635bbe0568af3
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2019-03-10 20:15:27 -0300

    Implementando projeto que viola OCP

---
+++ OCP/ClienteServidor_Sem_OCP/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ClienteServidor_Sem_OCP
+{
+    public class Client
+    {
+        public void TarefasDiaria()
+        {
+            Server server = new Server();
+
+            server.Ligar();
+
+            Trabalhar();
+
+            server.Desligar();
+        }
+
+        public void Trabalhar()
+        {
+            Console.WriteLine("Trabalhando durante o dia todo");
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ OCP/ClienteServidor_Sem_OCP/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClienteServidor_Sem_OCP
+{
+    internal class Server
+    {
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando servidor");
+        }
+
+        public void Desligar()
+        {
+            Console.WriteLine("Desligando servidor");
+        }
+    }
+}
