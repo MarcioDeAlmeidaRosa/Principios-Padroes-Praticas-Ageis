ce8f9b1b00541208b95119d4f2e47f6ef60e2a76
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2019-03-10 19:37:42 -0300

    Aplicando SRP no projeto de Geometria

---
+++ SRP/Geometria_Com_SRP/GeometricRetangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Geometria_Com_SRP
+{
+    public class GeometricRetangle
+    {
+        /// <summary>
+        /// Agora se o cliente de Retangle fizer Retangle mudar, essa alteração não afetará
+        /// o cliente de GeometricRetangle
+        /// </summary>
+        protected double Base { get; set; }
+        protected double Altura { get; set; }
+
+        public double Area()
+        {
+            Console.WriteLine("Fornecer um modelo matemático da geometria de um retângulo");
+            return Base * Altura;
+        }
+    }
+}
---
+++ SRP/Geometria_Com_SRP/Retangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geometria_Com_SRP
+{
+    public class Retangle
+    {   
+        public void Draw()
+        {
+            Console.WriteLine("Representar o retêngulo em uma interface gráfica (GUI)");
+        }
+    }
+}
