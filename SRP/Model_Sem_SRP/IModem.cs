373961378f3d23f55f8c481c30dca55bd91ec6a4
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2019-03-10 19:52:12 -0300

    Exemplo de violação de SRP, pois qualquer alteração na forma de discagem ou na troca de mensagem entre moldens, implica em recompilação geral das funcionalidades.

---
+++ SRP/Model_Sem_SRP/IModem.cs
@@ -0,0 +1,10 @@
+﻿namespace Model_Sem_SRP
+{
+    public interface IModem
+    {
+        void Dial(string pno); //Discar
+        void Hangup(); //Desligar
+        void Send(char c); //Enviar
+        char Recv(); //Receber
+    }
+}
---
+++ SRP/Model_Sem_SRP/Modem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Model_Sem_SRP
+{
+    /// <summary>
+    /// Exemplo de violação da SRP
+    /// pois qualquer alteração na forma de discar e desligar for feita, 
+    /// terá que ser testado e distribuído o componente para a troca e recebimento
+    /// de mensagens
+    /// </summary>
+    public class Modem : IModem
+    {
+        public void Dial(string pno)
+        {
+            Console.WriteLine($"Efetuando discagem para ${pno}");
+        }
+
+        public void Hangup()
+        {
+            Console.WriteLine($"Desligando.");
+        }
+
+        public char Recv()
+        {
+            Console.WriteLine($"Recebendo");
+            return 'c';
+        }
+
+        public void Send(char c)
+        {
+            Console.WriteLine($"Enviando mensagem ${c}");
+        }
+    }
+}
