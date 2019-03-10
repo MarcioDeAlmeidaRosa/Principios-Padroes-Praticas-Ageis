e30f374ea02fca225980a849d461ca9c31b3f456
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2019-03-10 19:30:21 -0300

    Implementando um projeto que viola o SRP

---
+++ SRP/Geometria_Sem_SRP/Retangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Geometria_Sem_SRP
+{
+    public class Retangle
+    {
+        /// <summary>
+        /// Classe retangle tem duas responsabilidades
+        /// 1º Fornecer um modelo matemático da geometria de um retângulo
+        /// 2º Representar o retêngulo em uma interface gráfica
+        /// </summary>
+        protected double Base { get; set; }
+        protected double Altura { get; set; }
+
+        public void Draw()
+        {
+            Console.WriteLine("Representar o retêngulo em uma interface gráfica (GUI)");
+        }
+
+        public double Area()
+        {
+            Console.WriteLine("Fornecer um modelo matemático da geometria de um retângulo");
+            return Base * Altura;
+        }
+    }
+}
